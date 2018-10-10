55bd91de37802ffda0e22a628ad03f2ca5e8528e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-10 13:55:36 -0700

    Week 6 10/10/18

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -16,7 +16,24 @@ public class AnimalFarm {
 		/*
 		 * 1. Ask the user which animal they want, then play the sound of that animal.
 		 */
-		
+		for(int i=0; i>-1;i++) {
+		String animal=JOptionPane.showInputDialog("Which animal do you like?\nCow, duck, dog, cat, llama");
+		if(animal.equalsIgnoreCase("cow")) {
+			playMoo();
+		}
+		if(animal.equalsIgnoreCase("duck")) {
+			playQuack();
+		}
+		if(animal.equalsIgnoreCase("dog")) {
+			playWoof();
+		}
+		if(animal.equalsIgnoreCase("cat")) {
+			playMeow();
+		}
+		if(animal.equalsIgnoreCase("llama")) {
+			playLlama();
+		}
+		}
 		/* 2. Make it so that the user can keep entering new animals. */
 	}
 
@@ -30,6 +47,13 @@ public class AnimalFarm {
 
 	void playWoof() {
 		playNoise(woofFile);
+	}
+	
+	void playMeow() {
+		playNoise(meowFile);
+	}
+	void playLlama() {
+		playNoise(llamaFile);
 	}
 
 	String quackFile = "quack.wav";
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -11,7 +11,7 @@ public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
-		
+
 		int randomNumber = randomMaker.nextInt(5);
 		
 		System.out.println(randomNumber);
